95826880ee542d5bcb2bfa31f9ebf794c30e1a07
Max Stoiber <mxstbr@github.com>
2019-05-30 12:31:48 +0200

    Much nicer wrapper for generative rendering

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,5 @@
+import React from "react";
+import prettyFormat from "pretty-format";
 import { configure } from "@storybook/react";
 import { setDefaults } from "react-storybook-addon-props-combinations";
 import "@storybook/addon-console";
@@ -9,10 +11,44 @@ function loadStories() {
 }
 
 setDefaults({
-  style: {
-    display: "inline-block",
-    marginRight: "24px",
-    marginBottom: "24px"
+  CombinationRenderer: ({ Component, props }) => {
+    const el = <Component {...props} />;
+    return (
+      <div
+        style={{
+          display: "inline-block",
+          border: "1px solid #E9ECF0",
+          borderRadius: "3px",
+          margin: "16px",
+          boxShadow: "0px 1px 8px rgba(0, 0, 0, 0.08)"
+        }}
+      >
+        <div
+          style={{
+            padding: "24px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          {el}
+        </div>
+        <pre
+          style={{
+            padding: "24px",
+            background: "#002b36",
+            color: "#93a1a1",
+            margin: 0,
+            borderRadius: "0 0 3px 3px"
+          }}
+        >
+          {prettyFormat(el, {
+            plugins: [prettyFormat.plugins.ReactElement]
+          })}
+        </pre>
+      </div>
+    );
   }
 });
 
