8392697921ca37fd50e658dc47a8401d38d84762
Max Stoiber <mxstbr@github.com>
2019-05-30 12:18:02 +0200

    Render prop combinations inline

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from "@storybook/react";
+import { setDefaults } from "react-storybook-addon-props-combinations";
 import "@storybook/addon-console";
 
 // automatically import all files ending in *.stories.js
@@ -6,5 +7,13 @@ const req = require.context("../src", true, /\.stories\.(js|tsx?)$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+setDefaults({
+  style: {
+    display: "inline-block",
+    marginRight: "24px",
+    marginBottom: "24px"
+  }
+});
 
 configure(loadStories, module);
