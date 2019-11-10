e3d9c1790be31b57caa7eea53f078560b3779eee
Max Stoiber <mxstbr@github.com>
2019-11-10 16:01:34 +0100

    Fix decorator

--- src/Nav/Nav.stories.tsx
+++ src/Nav/Nav.stories.tsx
@@ -4,35 +4,31 @@ import { storiesOf, addDecorator } from "@storybook/react";
 import { Nav } from "./Nav";
 import { PrimaryButton } from "../Button";
 
-addDecorator(storyFn => (
+const ChangefeedNav = () => (
   <div style={{ height: "200vh" }}>
-    {storyFn()}
+    <Nav>
+      <Box pr={3}>
+        <strong>Some</strong>product
+      </Box>
+      <Nav.Content>
+        <Flex flexDirection={["column", "row", "row"]}>
+          <Nav.Item href="#">About</Nav.Item>
+          <Nav.Item href="#">Our Story</Nav.Item>
+          <Nav.Item href="#">Pricing</Nav.Item>
+        </Flex>
+        <Flex
+          pt={[2, 0]}
+          pb={[3, 0]}
+          justifyContent={["center", ""]}
+          width={[1, "auto"]}
+        >
+          <PrimaryButton>Get started</PrimaryButton>
+        </Flex>
+      </Nav.Content>
+    </Nav>
 
     <h1>Navbar</h1>
   </div>
-));
-
-const ChangefeedNav = () => (
-  <Nav>
-    <Box pr={3}>
-      <strong>Some</strong>product
-    </Box>
-    <Nav.Content>
-      <Flex flexDirection={["column", "row", "row"]}>
-        <Nav.Item href="#">About</Nav.Item>
-        <Nav.Item href="#">Our Story</Nav.Item>
-        <Nav.Item href="#">Pricing</Nav.Item>
-      </Flex>
-      <Flex
-        pt={[2, 0]}
-        pb={[3, 0]}
-        justifyContent={["center", ""]}
-        width={[1, "auto"]}
-      >
-        <PrimaryButton>Get started</PrimaryButton>
-      </Flex>
-    </Nav.Content>
-  </Nav>
 );
 
 storiesOf("Nav", module)
